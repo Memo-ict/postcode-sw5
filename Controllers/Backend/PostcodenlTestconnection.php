73a6c9f3927f79b1757c941872025f1067c85ddd
Daniel <daniel@memo-ict.nl>
2020-07-01 12:30:41 +0200

    Fixed always OK in configuration

--- Controllers/Backend/PostcodenlTestconnection.php
+++ Controllers/Backend/PostcodenlTestconnection.php
@@ -13,7 +13,9 @@ Class Shopware_Controllers_Backend_PostcodenlTestconnection extends \Shopware_Co
 
         if(empty($apiKey) || empty($apiSecret) )
         {
-            $this->View()->assign('response', 'You have not filled in all required fields, please check your input.');
+            $this->View()
+                ->assign('responseType', 'Error')
+                ->assign('response', 'You have not filled in all required fields, please check your input and save the form before testing the connection.');
             return;
         }
 
@@ -23,14 +25,20 @@ Class Shopware_Controllers_Backend_PostcodenlTestconnection extends \Shopware_Co
             $result = $client->accountInfo();
 
             if($result['hasAccess'] == true) {
-                $this->View()->assign('response', 'Succes, Your connection works');
+                $this->View()
+                    ->assign('responseType', 'Success')
+                    ->assign('response', 'Succes, Your connection works');
             } else {
-                $this->View()->assign('response', 'Something went wrong, check plugin log for complete error');
-                $this->get('pluginlogger')->addError('There is a error communicating with the Postcode.nl API please contact Postcode.nl servicedesk');
+                $this->View()
+                    ->assign('responseType', 'Error')
+                    ->assign('response', 'Something went wrong, check plugin log for complete error');
+                $this->get('pluginlogger')->addError('There is an error communicating with the Postcode.nl API, please contact Postcode.nl servicedesk');
             }
         } catch (\PostcodeNl\Api\Exception\ClientException $e) {
             $this->get('pluginlogger')->addError($e->getMessage());
-            $this->View()->assign('response', $e->getMessage());
+            $this->View()
+                ->assign('responseType', 'Error')
+                ->assign('response', $e->getMessage());
         }
     }
 }
